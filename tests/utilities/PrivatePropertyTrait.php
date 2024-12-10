dbb0f33b779f6842d1fc475791107d3e19bddb49
Miranda Hawarden-Ogata <hawarden@hawaii.edu>
2024-12-09 20:58:35 -1000

    Add tests/utilities for test-specific utility classes

---
+++ tests/utilities/PrivatePropertyTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\utilities;
+
+/**
+ * Trait for accessing private/protected properties in unit tests.
+ *
+ * NOTE: This trait is intended exclusively for use in test classes and
+ * should never be used in production code.
+ */
+trait PrivatePropertyTrait
+{
+    /**
+     * Accesses a private or protected property of an object for testing purposes.
+     *
+     * @param object $object   The object instance.
+     * @param string $property The name of the property to access.
+     *
+     * @return mixed The value of the property.
+     */
+    private function getPrivateProperty(object $object, string $property)
+    {
+        $reflection = new \ReflectionClass($object);
+        $propertyReflection = $reflection->getProperty($property);
+        $propertyReflection->setAccessible(true);
+        return $propertyReflection->getValue($object);
+    }
+}
---
+++ tests/utilities/README.md
@@ -0,0 +1,5 @@
+# Test Utilities
+
+This directory contains reusable utilities for the test suite, such as traits,
+helpers, and mock factories. These tools are designed to simplify and standardize
+the creation and management of tests. They are **not intended for production use.**
