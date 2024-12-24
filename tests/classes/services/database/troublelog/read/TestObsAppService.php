c4b4eb2804a820e63140f56b85f590c8282276be
Miranda Hawarden-Ogata <hawarden@hawaii.edu>
2024-12-23 18:54:20 -1000

    Refactor query generator method

--- tests/classes/services/database/troublelog/read/TestObsAppService.php
+++ tests/classes/services/database/troublelog/read/TestObsAppService.php
@@ -19,24 +19,39 @@ class TestObsAppService extends BaseService
     }
 
     /**
+     * Proxy method for testing the protected getProposalQuery method.
+     *
+     * @param string $condition The WHERE clause to use:
+     *                          - 'program':  Filter by semesterYear, semesterCode, and ProgramNumber.
+     *                          - 'semester': Filter by semesterYear and semesterCode.
+     *                          - 'session':  Filter by ObsApp_id.
+     *
+     * @return string The SQL query string.
+     */
+    public function getProposalQueryProxy(string $condition = 'session'): string
+    {
+        return $this->getProposalQuery($condition);
+    }
+
+    /**
      * Proxy method for testing the protected getProposalListingFormDataQuery method.
      *
      * @param bool $sortAsc Determines 'ASC' or 'DESC' sort.
      *
      * @return string The SQL query string.
-     */
+     *//*
     public function getProposalListingFormDataQueryProxy(bool $semester): string
     {
         return $this->getProposalListingFormDataQuery($semester);
-    }
+    }*/
 
     /**
      * Proxy method for testing the protected getProposalProgramDataQuery method.
      *
      * @return string The params types string.
-     */
+     *//*
     public function getProposalProgramDataQueryProxy(): string
     {
         return $this->getProposalProgramDataQuery();
-    }
+    }*/
 }
