665ee6f4d3b05131c2097669b781f0c815643645
Miranda Hawarden-Ogata <hawarden@hawaii.edu>
2024-12-29 18:26:00 -1000

    Reorganise and rename MockDBConnectionTrait

--- tests/utilities/mocks/MockDBConnectionTrait.php
+++ tests/utilities/mocks/MockDBConnectionTrait.php
@@ -2,23 +2,41 @@
 
 declare(strict_types=1);
 
-namespace Tests\utilities;
+namespace Tests\utilities\mocks;
 
 use Mockery;
 
 /**
  * Trait for mocking the DBConnection class in unit tests.
  *
+ * This trait provides a mock for the `DBConnection` class, commonly used for managing
+ * database connections. It allows the definition of default behaviors for frequently
+ * used methods and supports test-specific overrides. Additionally, it includes a helper
+ * method for setting expectations on `getInstance` calls.
+ *
+ * Features:
+ * - Mocks the `DBConnection` class with configurable behaviors.
+ * - Provides helper methods to set expectations for connection retrieval.
+ * - Simplifies database-related unit testing.
+ *
+ * Example usage:
+ * ```
+ * $mockDBConnection = $this->createDBConnectionMock(['getError' => 'Mocked error']);
+ * $this->mockGetInstance($mockDBConnection, 'test_db');
+ * ```
+ *
  * NOTE: This trait is intended exclusively for use in test classes and
  * should never be used in production code.
  */
-trait DBConnectionMockTrait
+trait MockDBConnectionTrait
 {
     /**
      * Creates a mock of the DBConnection class with predefined behavior.
      *
-     * @param array $methodResponses Array of methods and their return values.
-     * @return Mockery\MockInterface Mocked DBConnection instance.
+     * @param array $methodResponses An array of method names mapped to their return values
+     *                               or callables for custom behavior.
+     *
+     * @return Mockery\MockInterface The mocked DBConnection instance.
      */
     protected function createDBConnectionMock(array $methodResponses = []): Mockery\MockInterface
     {
@@ -40,8 +58,10 @@ trait DBConnectionMockTrait
     /**
      * Sets up a mock expectation for the `getInstance` method.
      *
-     * @param Mockery\MockInterface $dbMock The mocked DB connection.
-     * @param string                $dbName Database name.
+     * This helper method defines expectations for retrieving a specific database instance.
+     *
+     * @param Mockery\MockInterface $dbMock The mocked DBConnection instance.
+     * @param string                $dbName The name of the database to retrieve.
      *
      * @return void
      */
