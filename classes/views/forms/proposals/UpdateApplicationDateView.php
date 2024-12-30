f483e0e3a92b99dec01b517ad9a82fe85f11ce02
Miranda Hawarden-Ogata <hawarden@hawaii.edu>
2024-12-30 00:34:56 -1000

    Update to extend BaseFormView

--- classes/views/forms/proposals/UpdateApplicationDateView.php
+++ classes/views/forms/proposals/UpdateApplicationDateView.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace App\views\forms\proposals;
 
-use App\core\common\Debug;
-use App\views\forms\BaseFormView as BaseView;
+use App\core\common\CustomDebug;
+use App\exceptions\HtmlBuilderException;
+use App\views\forms\BaseFormView          as BaseView;
+use App\core\htmlbuilder\HtmlBuilder      as HtmlBuilder;
+use App\core\htmlbuilder\CompositeBuilder as CompBuilder;
+use App\legacy\IRTFLayout                 as IrtfBuilder;
 
 /**
  * View for rendering the Update Application Date form.
@@ -21,23 +25,38 @@ class UpdateApplicationDateView extends BaseView
     /**
      * Initializes the UpdateApplicationDateView with core builders and configurations.
      *
-     * @param bool  $formatHtml Whether to format the HTML output.
-     * @param Debug $debug      Optional. Debugging utility instance.
+     * @param bool|null        $formatHtml  Enable formatted HTML output. Defaults to false if not provided.
+     * @param CustomDebug|null $debug       Debug instance for logging and debugging. Defaults to a new Debug instance.
+     * @param HtmlBuilder|null $htmlBuilder Instance for constructing HTML elements. Defaults to a new HtmlBuilder.
+     * @param CompBuilder|null $compBuilder Instance for composite HTML elements. Defaults to a new CompBuilder.
+     * @param IrtfBuilder|null $irtfBuilder Legacy layout builder for site meta. Defaults to a new IrtfBuilder.
      */
     public function __construct(
-        bool $formatHtml = false,
-        ?Debug $debug = null
+        ?bool $formatHtml = null,
+        ?CustomDebug $debug = null,
+        ?HtmlBuilder $htmlBuilder = null, // Dependency injection to simplify unit testing
+        ?CompBuilder $compBuilder = null, // Dependency injection to simplify unit testing
+        ?IrtfBuilder $irtfBuilder = null  // Dependency injection to simplify unit testing
     ) {
         // Use parent class' constructor
-        parent::__construct($formatHtml ?? false, $debug);
+        parent::__construct($formatHtml, $debug, $htmlBuilder, $compBuilder, $irtfBuilder);
         $debugHeading = $this->debug->debugHeading("View", "__construct");
         $this->debug->debug($debugHeading);
-        $this->debug->log("{$debugHeading} -- Parent class is successfully constructed.");
+        $this->debug->debug("{$debugHeading} -- Parent class is successfully constructed.");
 
         // Class initialisation complete
-        $this->debug->log("{$debugHeading} -- View initialisation complete.");
+        $this->debug->debug("{$debugHeading} -- View initialisation complete.");
     }
 
+    // Abstract methods: getFieldLabels(), getPageContents()
+
+    /**
+     * Provides field labels for the Login form.
+     *
+     * Maps internal field names to user-friendly labels.
+     *
+     * @return array An associative array mapping field names to labels.
+     */
     public function getFieldLabels(): array
     {
         // Debug output
@@ -48,6 +67,19 @@ class UpdateApplicationDateView extends BaseView
         return [];
     }
 
+    /**
+     * Generates the main page content for the update form.
+     *
+     * This method defines the specific HTML structure for the login form,
+     * using the provided database and form data. The BaseFormView parent method
+     * getContentsForm() passes the contents to renderFormPage(), etc., for rendering.
+     *
+     * @param array $dbData   Data arrays required to populate form options. Defaults to an empty array.
+     * @param array $formData Default data for form fields. Defaults to an empty array.
+     * @param int   $pad      Optional padding level for formatted output. Defaults to 0.
+     *
+     * @return string The HTML content for the form page.
+     */
     protected function getPageContents(
         array $dbData = [],
         array $formData = [],
@@ -56,8 +88,11 @@ class UpdateApplicationDateView extends BaseView
         // Debug output
         $debugHeading = $this->debug->debugHeading("View", "getPageContents");
         $this->debug->debug($debugHeading);
+        $this->debug->debugVariable($dbData, "{$debugHeading} -- dbData");
+        $this->debug->debugVariable($formData, "{$debugHeading} -- formData");
+        $this->debug->debugVariable($pad, "{$debugHeading} -- pad");
 
-        // Generate the main page content for a form
+        // Build the page contents
         return '';
     }
 
