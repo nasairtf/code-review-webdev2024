d45d2fdaf9c26b52bcb7c3d260650de9d9513d74
Miranda Hawarden-Ogata <hawarden@hawaii.edu>
2025-01-10 18:18:06 -1000

    Refactor to use GenerateTACScoreResultsFormPageTrait

--- classes/views/forms/proposals/UploadTACScoresView.php
+++ classes/views/forms/proposals/UploadTACScoresView.php
@@ -11,6 +11,7 @@ use App\core\htmlbuilder\HtmlBuilder      as HtmlBuilder;
 use App\core\htmlbuilder\CompositeBuilder as CompBuilder;
 use App\legacy\IRTFLayout                 as IrtfBuilder;
 use App\core\irtf\IrtfLinks;
+use App\legacy\traits\GenerateTACScoreResultsFormPageTrait;
 
 /**
  * View for rendering the TAC scores upload form.
@@ -23,6 +24,8 @@ use App\core\irtf\IrtfLinks;
 
 class UploadTACScoresView extends BaseView
 {
+    use GenerateTACScoreResultsFormPageTrait;
+
     private $irtfLinks;
 
     /**
@@ -107,66 +110,6 @@ class UploadTACScoresView extends BaseView
         $this->debug->debugVariable($pad, "{$debugHeading} -- pad");
 
         // Build the page contents
-        $code  = "";
-        $color = "";
-        $first = 2001;
-        $year  = date( "Y", time() );
-
-        $code .= "<table width='100%' border='0' cellspacing='0' cellpadding='6'>\n";
-        $code .= getHorizontalLine(0, 2, "FFFFFF");
-
-        $height = 45;
-        $width = 75;
-        $bwid = 120;
-        $color = getGrayShading( $color );
-        $code .= "  <tr bgcolor='#{$color}' height='{$height}'>\n";
-        $code .= "     <td colspan='2' align='center'>Select the semester:</td>\n";
-        $code .= "  </tr>\n";
-
-        $color = getGrayShading( $color );
-        $code .= "  <tr bgcolor='#{$color}' height='{$height}'>\n";
-        $code .= "    <td colspan='2' align='center'>\n";
-        $code .= "      Year:&nbsp;\n";
-        $code .= getPulldownNumbers( "y", $year, 4, $first, $year + 1 );
-        $code .= "      &nbsp;&nbsp;&nbsp;\n";
-        $code .= "      Semester:&nbsp;\n";
-        $code .= getPulldownSemesters( "s", "", 4 );
-        $code .= "    </td>\n";
-        $code .= "  </tr>\n";
-
-        $color = getGrayShading($color);
-        $code .= "  <tr bgcolor='#{$color}' height='{$height}'>\n";
-        $code .= "    <td align='right'>\n";
-        $code .= "      Solar System TAC results/time allocation file:\n";
-        $code .= "      <input type='hidden' name='MAX_FILE_SIZE' value='60000000' >\n";
-        $code .= "    </td>\n";
-        //$code .= "  </tr>\n";
-        //$code .= "  <tr bgcolor='#{$color}' height='{$height}'>\n";
-        $code .= "    <td align='left'><input type='file' name='tacss' /></td>\n";
-        $code .= "  </tr>\n";
-
-        $color = getGrayShading($color);
-        $code .= "  <tr bgcolor='#{$color}' height='{$height}'>\n";
-        $code .= "    <td align='right'>\n";
-        $code .= "      Non-Solar System TAC results/time allocation file:\n";
-        $code .= "      <input type='hidden' name='MAX_FILE_SIZE' value='60000000' >\n";
-        $code .= "    </td>\n";
-        //$code .= "  </tr>\n";
-        //$code .= "  <tr bgcolor='#{$color}' height='{$height}'>\n";
-        $code .= "    <td align='left'><input type='file' name='tacnss' /></td>\n";
-        $code .= "  </tr>\n";
-
-        $color = getGrayShading( $color );
-        $code .= "  <tr bgcolor='#{$color}' height='{$height}'>\n";
-        $code .= "    <td colspan='2' align='center'>\n";
-        $code .= "      <input type='submit' name='clear' value='Clear' style='width: {$bwid}px;'/>\n";
-        $code .= "      <input type='submit' name='submit' value='Upload Files' style='width: {$bwid}px;'/>\n";
-        $code .= "    </td>\n";
-        $code .= "  </tr>\n";
-
-        $code .= getHorizontalLine( 0, 2, "FFFFFF" );
-        $code .= "</table>\n";
-
-        return $code;
+        return $this->generateTacResultsFormPage();
     }
 }
