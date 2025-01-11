e755112e5b856942c3f9c5031dbf64d1e028a4d4
Miranda Hawarden-Ogata <hawarden@hawaii.edu>
2025-01-10 20:58:04 -1000

    Add form to handle upload of csv file exported from Filemaker

---
+++ classes/legacy/traits/GenerateTACResultsFilemakerFormPageTrait.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\legacy\traits;
+
+trait GenerateTACResultsFilemakerFormPageTrait
+{
+    ############################################################################
+    #
+    # Generates the form to upload the TAC results file exported from FMP
+    #
+    #---------------------------------------------------------------------------
+    #
+    private function generateTacResultsFormPage(): string
+    {
+        $code  = "";
+        $color = "";
+        $first = 2001;
+        $year  = date( "Y", time() );
+
+        $code .= "<table width='100%' border='0' cellspacing='0' cellpadding='6'>\n";
+        $code .= getHorizontalLine(0, 2, "FFFFFF");
+
+        $height = 45;
+        $width = 75;
+        $bwid = 120;
+        $color = getGrayShading( $color );
+        $code .= "  <tr bgcolor='#{$color}' height='{$height}'>\n";
+        $code .= "     <td colspan='2' align='center'>Select the semester:</td>\n";
+        $code .= "  </tr>\n";
+
+        $color = getGrayShading( $color );
+        $code .= "  <tr bgcolor='#{$color}' height='{$height}'>\n";
+        $code .= "    <td colspan='2' align='center'>\n";
+        $code .= "      Year:&nbsp;\n";
+        $code .= getPulldownNumbers( "y", $year, 4, $first, $year + 1 );
+        $code .= "      &nbsp;&nbsp;&nbsp;\n";
+        $code .= "      Semester:&nbsp;\n";
+        $code .= getPulldownSemesters( "s", "", 4 );
+        $code .= "    </td>\n";
+        $code .= "  </tr>\n";
+
+        $color = getGrayShading($color);
+        $code .= "  <tr bgcolor='#{$color}' height='{$height}'>\n";
+        $code .= "    <td align='right'>\n";
+        $code .= "      TAC results/time allocation Filemaker file:\n";
+        $code .= "      <input type='hidden' name='MAX_FILE_SIZE' value='60000000' >\n";
+        $code .= "    </td>\n";
+        //$code .= "  </tr>\n";
+        //$code .= "  <tr bgcolor='#{$color}' height='{$height}'>\n";
+        $code .= "    <td align='left'><input type='file' name='tac' /></td>\n";
+        $code .= "  </tr>\n";
+
+        $color = getGrayShading( $color );
+        $code .= "  <tr bgcolor='#{$color}' height='{$height}'>\n";
+        $code .= "    <td colspan='2' align='center'>\n";
+        $code .= "      <input type='submit' name='clear' value='Clear' style='width: {$bwid}px;'/>\n";
+        $code .= "      <input type='submit' name='submit' value='Upload File' style='width: {$bwid}px;'/>\n";
+        $code .= "    </td>\n";
+        $code .= "  </tr>\n";
+
+        $code .= getHorizontalLine( 0, 2, "FFFFFF" );
+        $code .= "</table>\n";
+
+        return $code;
+    }
+    #---------------------------------------------------------------------------
+    #-- end of generateTacResultsFormPage
+    ############################################################################
+}
